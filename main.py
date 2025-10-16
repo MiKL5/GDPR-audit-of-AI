55a191672f7b73ca0a3e34ec36fc343bccc61d12
MiKL5 <miklgaillard@gmail.com>
2025-10-16 18:06:08 +0200

    🎉 Creation of AI

---
+++ main.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import numpy     as np
+import joblib
+
+# Le modèle pré-entraîné
+with open("rfr_model.pkl", "rb") as model_file:
+    model = joblib.load(model_file)
+
+st.markdown("""<h1 style="text-align:center;color:green;">Prédiction du prix d'une maison 🏠 en Californie</h1>""", unsafe_allow_html=True)
+st.markdown("""<div style="text-align:center;">Choisissez les caractéristiques<br><br></div>""",                    unsafe_allow_html=True)
+
+# Les variables
+longitude          = st.number_input("Quelle est la longitude ? (de -114 à -124)",                          min_value=-124.0, max_value=-114.0, value=-120.0)
+latitude           = st.number_input("Quelle est la latitude ? (de 32 à 42)",                               min_value=32.0,   max_value=42.0,   value=34.0)
+housing_median_age = st.slider("Choisissez l'age moyen des maisons (années) ? (Jusqu'à 50 ans)",            min_value=1,      max_value=50,     value=20)
+total_rooms        = st.slider("Quel est le nombre moyen de pièces (d'1 à 10 pièces)",                      min_value=1.0,    max_value=10.0,   value=6.0)
+total_bedrooms     = st.slider("Quel est le nombre moyen de chambres (d'1 à 5)",                            min_value=1.0,    max_value=5.0,    value=1.0)
+population         = st.number_input("Quelle serait la population du quartier ? (de 10 à 5000 habitants)",  min_value=10,     max_value=5000,   value=500)
+households         = st.slider("Quelle est l'occupation moyenne ? (de 1 à 10)",                             min_value=1.0,    max_value=10.0,   value=3.0)
+median_income      = st.number_input("Quel votre revenu médian (en dizaines de milliers de $) ? (de 0 20)", min_value=0.0,    max_value=20.0,   value=4.0)
+
+# Variables catégorielles
+ocean_proximity_options = ['<1H OCEAN', 'INLAND', 'ISLAND', 'NEAR BAY', 'NEAR OCEAN']
+ocean_proximity         = st.selectbox("Proximité de l'océan", ocean_proximity_options)
+ocean_prox_encoded      = [1 if ocean_proximity == option else 0 for option in ocean_proximity_options]
+
+# Bouton de prédiction
+if st.button("Prédire le prix"):
+    # Déclarer toutes les variables dans l'ordre d'entraînement
+    X_new = np.array([[longitude, latitude, housing_median_age, total_rooms, total_bedrooms, population, households, median_income] + ocean_prox_encoded])
+    prediction = model.predict(X_new)
+    
+    # Affichage centré et stylisé
+    st.markdown(f"""<div style="text-align:center; font-weight:bold; color:green; margin-top:10px;">Prix médian éstimé 👉 {prediction[0]:,.2f} $</div>""", unsafe_allow_html=True)
+
+st.markdown("</div>", unsafe_allow_html=True)
+st.markdown("""
+<div style="background-color:#FFFACD;padding:15px 15px;border-radius:50px;color:#00008B;font-weight:extra-bold;font-size:20px;text-align:center;marign:auto;">
+👀 Aucune donnée personnelle n'est collectée ou stockée.<br>Les entrées saisies ne servent qu'à la prédiction. 👀</div>
+""", unsafe_allow_html=True)
---
+++ readme.md
@@ -0,0 +1,47 @@
+# Audit RGPD d'une IA de prédiction du prix des maisons 🏠<!-- <a href="../../"><img align="right" src="https://upload.wikimedia.org/wikipedia/commons/5/5d/JetBrains_PyCharm_Product_Logo.svg" alt="PyCharm" height="64px"></a> -->
+
+<div align="center">
+
+![Python](https://img.shields.io/badge/python-3.13-blue?style=flat&logo=python&logoColor=ffd43b) 
+![Streamlit](https://img.shields.io/badge/Streamlit-Data_App-FF4B4B?style=flat&logo=streamlit&logoColor=white) 
+![NumPy](https://img.shields.io/badge/NumPy-1.25-blue?style=flat&logo=NumPy&logoColor=white) 
+![Joblib](https://img.shields.io/badge/Joblib-Model_Loading-0078D4?style=flat) 
+<!-- ![MIT License](https://img.shields.io/badge/License-MIT-blue.svg)  -->
+
+</div><hr>
+
+## **Le projet**
+Cette application web interactive, développée avec **Streamlit**, permet d’estimer le prix médian d’une maison en Californie à partir de ses caractéristiques géographiques et démographiques.  
+Le modèle de prédiction est un modèle de forêt aléatoire (`RandomForestRegressor`) pré-entraîné et sauvegardé, chargé via **joblib**.
+---
+## Fonctionnalités principales
+* Interface utilisateur intuitive avec des champs de saisie numériques et sliders pour les caractéristiques suivantes :
+  * Longitude et latitude  
+  * Âge médian des maisons  
+  * Nombre moyen de pièces et chambres  
+  * Population locale et nombre de ménages  
+  * Revenu médian du quartier  
+  * Proximité de l'océan (catégorielle)  
+* Prédiction instantanée via un bouton dédié, affichage clair et stylisé du prix médian estimé.
+* Protection de la vie privée : aucune donnée personnelle n’est collectée ou stockée.
+## Structure du projet
+```txt
+├── rfr_model.pkl # Modèle pré-entraîné (Random Forest)
+├── app.py # Script principal Streamlit (cette application)
+├── README.md # Documentation
+└── requirements.txt # Dépendances nécessaires
+```
+## Lancer l'application
+```bash
+streamlit run main.py
+```
+Le navigateur ouvrira l’interface.  
+Vous pourrez ajuster les paramètres et obtenir la prédiction du prix.
+
+## **Avertissements**
+* Ce modèle est un prototype pédagogique et ne garantit pas une précision parfaite en conditions réelles.
+* Les données saisies ne sont pas stockées, respectant ainsi la confidentialité de l'utilisateur.
+___
+## Auteurs
+Quentin HECQUET
+Mickael Gaillard
